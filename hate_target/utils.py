8c842d59b8fe21272ee071196028ad4aaadc3aef
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2022-02-03 14:52:52 -0800

    remove functions from utils, add custom cv wrapper

--- hate_target/utils.py
+++ hate_target/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow_hub as hub
 
-from sklearn.model_selection import GroupKFold, GroupShuffleSplit
+from sklearn.model_selection import KFold, ShuffleSplit
 from tensorflow.keras.optimizers import Adam
 
 
@@ -20,88 +20,12 @@ def accuracy_by_chance(p_train, p_test):
     return 2 * p_train * p_test - p_train - p_test + 1
 
 
-def cv_probs_to_preds_multi_output(probs):
-    """Converts probabilities to prediction labels for multi-output data.
-
-    This function assumes the input is a nested list, with outer list
-    corresponding to cross-validation folds, and inner list corresponding to
-    outputs.
-
-    Parameters
-    ----------
-    probs : list
-        A list of lists. The outer list corresponds to cross-validation folds,
-        while the inner list contains the multi-output probability predictions.
-        Within the inner list is a series of np.ndarrays, with first dimension
-        corresponding to number of samples, and second dimension corresponding
-        to number of categories for that output.
-
-    Returns
-    -------
-    preds : list
-        A list of np.ndarrays, each corresponding to cross-validation
-        folds. Each np.ndarray has first dimension matching the number of
-        samples, and the second dimension matching the number of outputs.
-    """
-    n_folds = len(probs)
-    n_outputs = len(probs[0])
-
-    preds = [np.array([np.argmax(probs[fold][output], axis=1)
-                       for output in range(n_outputs)]).T
-             for fold in range(n_folds)]
-    return preds
-
-
-def examples_to_features(batch, tokenizer, max_length=None, padding='max_length',
-                         return_tensors='np', **kwargs):
-    """Wrapper function for tokenizing a batch of input examples.
-
-    By default, this wrapper pads to max length, as set by the tokenizer. It
-    returns the encoded features as tensors. Lastly, it separates the input
-    IDs of the tokens and the attention masks.
-
-    Parameters
-    ----------
-    batch : np.ndarray
-        Array of text inputs, with shape equal to the batch size.
-    tokenizer : Tokenizer
-        Tokenizer as obtained from the transformers package.
-    max_length : int or None
-        The maximum length of the input string. If None, is setting according
-        to the tokenizer.
-    padding : int, string, or bool
-        If int, the length to which input strings are padded. If 'max_length',
-        strings are padded to the maximum length. If True, strings are padded
-        to maximum observed string.
-    return_tensors : string
-        The format for which to return the encoded tensors. By default, returns
-        Tensorflow tensors.
-    **kwargs : input arguments
-        Other arguments to pass to the tokenizer.
-
-    Returns
-    -------
-    input_ids : tf.Tensor
-        The tokenized input IDs for each input string.
-    attention_masks : tf.Tensor
-        The attention mask for each input string.
-    """
-    encoded = tokenizer.batch_encode_plus(batch,
-                                          max_length=max_length,
-                                          padding=padding,
-                                          return_tensors=return_tensors,
-                                          **kwargs)
-    input_ids = encoded['input_ids']
-    attention_masks = encoded['attention_mask']
-    return input_ids, attention_masks
-
-
-def group_cv_wrapper(x, y, model_builder, model_kwargs={}, compile_kwargs={},
-                     batch_size=32, max_epochs=50, n_folds=5, val_frac=0.2,
-                     lr=0.001, refit=False, refit_fold=False,
-                     unwrap_predictions=False, report_chance=False, groups=None,
-                     callbacks=None, sample_weights=None, random_state=None,
-                     verbose=False, cv_verbose=False):
+def cv_wrapper(x, y, model_builder, model_kwargs={}, compile_kwargs={},
+               batch_size=32, max_epochs=50, n_folds=5, val_frac=0.2,
+               lr=0.001, refit=False, refit_fold=False,
+               unwrap_predictions=False, report_chance=False,
+               callbacks=None, sample_weights=None, random_state=None,
+               verbose=False, cv_verbose=False):
     """Cross-validate a model and evaluate its predictive accuracy.
 
     Within each fold, the model is fit first with early stopping on a
@@ -141,9 +65,6 @@ def group_cv_wrapper(x, y, model_builder, model_kwargs={}, compile_kwargs={},
     refit_fold : bool
         If True, refits the model within the fold to the entire training set
         using the early stopping number of epochs.
-    groups : array-like or None
-        The group identification for each sample. If None, assumes all samples
-        are in the same group.
     unwrap_predictions : bool
         If True, rearranges the test predictions to have outer list correspond
         to output, while each inner array corresponds to the predictions for
@@ -190,13 +111,10 @@ def group_cv_wrapper(x, y, model_builder, model_kwargs={}, compile_kwargs={},
     # Use the first input to extract the sample
     n_samples = x[0].shape[0]
     n_outputs = len(y)
-    # Edge case for groups and weights
-    if groups is None:
-        groups = np.ones(n_samples)
     if sample_weights is None:
         sample_weights = np.ones(n_samples)
     # Create cross-validation object
-    cv = GroupKFold(n_splits=n_folds)
+    cv = KFold(n_splits=n_folds)
     # Lists for storing information across folds
     n_epochs = np.zeros(n_folds, dtype=int)
     if report_chance:
@@ -217,7 +135,7 @@ def group_cv_wrapper(x, y, model_builder, model_kwargs={}, compile_kwargs={},
         print("Beginning cross-validation...")
 
     # Iterate over group folds
-    for fold_idx, (train_idx, test_idx) in enumerate(cv.split(x[0], groups=groups)):
+    for fold_idx, (train_idx, test_idx) in enumerate(cv.split(x[0])):
         if cv_verbose:
             print(f"Fold {fold_idx + 1} / {n_folds}")
         # Store training and testing indices
@@ -226,14 +144,13 @@ def group_cv_wrapper(x, y, model_builder, model_kwargs={}, compile_kwargs={},
         # Create training set for current fold
         x_train = [x_input[train_idx] for x_input in x]
         y_train = [y_output[train_idx] for y_output in y]
-        groups_train = groups[train_idx]
         sample_weights_train = sample_weights[train_idx]
         # Generate validation set, while respecting group structure
-        cv_val = GroupShuffleSplit(n_splits=1,
-                                   test_size=val_frac,
-                                   random_state=random_state)
+        cv_val = ShuffleSplit(n_splits=1,
+                              test_size=val_frac,
+                              random_state=random_state)
         # Obtain indices for single validation set by applying generator
-        train_val_idx, test_val_idx = next(cv_val.split(x_train[0], groups=groups_train))
+        train_val_idx, test_val_idx = next(cv_val.split(x_train[0]))
         x_train_val = [x_input[train_val_idx] for x_input in x_train]
         y_train_val = [y_output[train_val_idx] for y_output in y_train]
         x_test_val = [x_input[test_val_idx] for x_input in x_train]
@@ -258,7 +175,7 @@ def group_cv_wrapper(x, y, model_builder, model_kwargs={}, compile_kwargs={},
         n_epochs[fold_idx] = n_epochs_best
         if refit_fold:
             if cv_verbose:
-                print(f"Refitting to entire training set with {n_epochs[fold_idx]} folds.")
+                print(f"Refitting to entire training set with {n_epochs[fold_idx]} epochs.")
             model = model_builder(**model_kwargs)
             model.compile(**compile_kwargs)
             history = model.fit(x=x_train,
@@ -280,7 +197,7 @@ def group_cv_wrapper(x, y, model_builder, model_kwargs={}, compile_kwargs={},
         test_scores.append(model.evaluate(x=x_test, y=y_test))
         if report_chance:
             chance[fold_idx] = [
-                accuracy_by_chance(train[:, 1].mean(), test[:, 1].mean())
+                accuracy_by_chance(train.mean(), test.mean())
                 for train, test in zip(y_train, y_test)]
         if cv_verbose:
             print("Fold complete.")
@@ -319,30 +236,6 @@ def group_cv_wrapper(x, y, model_builder, model_kwargs={}, compile_kwargs={},
     if report_chance:
         results = results + (chance,)
     return results
-
-
-def initialize_use_layer(version):
-    """Initializes a Universal Sentence Encoder (USE) layer.
-
-    Parameters
-    ----------
-    version : string
-        Denotes the USE version to download, or the URL from which to directly
-        download the USE from.
-
-    Returns
-    -------
-    use : keras.layer
-        A Keras Layer operating as a Universal Sentence Encoder.
-    """
-    if version == 'v4':
-        url = "https://tfhub.dev/google/universal-sentence-encoder/4"
-    elif version == 'v5':
-        url = "https://tfhub.dev/google/universal-sentence-encoder-large/5"
-    else:
-        url = version
-    use = hub.KerasLayer(handle=url, trainable=False)
-    return use
 
 
 def weighted_correlation(x, y, weights):
